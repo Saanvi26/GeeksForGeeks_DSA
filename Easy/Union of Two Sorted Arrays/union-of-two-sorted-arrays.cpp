29566a9d515c4d5bd1291f7cae213bfbab15dd14
Saanvi Lakhanpal <129958210+Saanvi26@users.noreply.github.com>
2023-08-17 09:19:03 +0530

    Updated Solution - GfG to GitHub

--- Easy/Union of Two Sorted Arrays/union-of-two-sorted-arrays.cpp
+++ Easy/Union of Two Sorted Arrays/union-of-two-sorted-arrays.cpp
@@ -11,20 +11,43 @@ class Solution{
     //Function to return a list containing the union of the two arrays. 
     vector<int> findUnion(int arr1[], int arr2[], int n, int m)
     {
-        set<int>s;
-        for (int i=0;i<n;i++){
-            s.insert(arr1[i]);
-        }
-         for (int i=0;i<m;i++){
-            s.insert(arr2[i]);
-        }
-        vector<int>ans;
-       for (auto i : s){
-           ans.push_back(i);
+       int i=0;
+       int j=0;
+       vector<int>ans;
+       while(i<n && j<m){
+           if(arr1[i]<=arr2[j]){
+               if(ans.size()==0 || ans.back()!=arr1[i]){
+                   ans.push_back(arr1[i]);
+               }
+               i++;
+           }
+           else {
+                if(ans.size()==0 || ans.back()!=arr2[j]){
+                   ans.push_back(arr2[j]);
+               }
+               j++;
+           }
        }
-        return ans;
+       while(i<n){
+         
+               if(ans.size()==0 || ans.back()!=arr1[i]){
+                   ans.push_back(arr1[i]);
+               }
+               i++;
+           
+       }
+         while(j<m){
+         
+               if(ans.size()==0 || ans.back()!=arr2[j]){
+                   ans.push_back(arr2[j]);
+               }
+               j++;
+           
+       }
+       
+           
+       return ans;
     }
-    
 };
 
 //{ Driver Code Starts.
