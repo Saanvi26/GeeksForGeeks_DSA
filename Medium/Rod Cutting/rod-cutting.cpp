b5000ce82467ea7682647084449a91735ccc067e
Saanvi Lakhanpal <129958210+Saanvi26@users.noreply.github.com>
2024-01-13 18:00:30 +0530

    Updated Solution - GfG to GitHub

--- Medium/Rod Cutting/rod-cutting.cpp
+++ Medium/Rod Cutting/rod-cutting.cpp
@@ -7,10 +7,9 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-
 class Solution{
     private:
-    int maxprofit(int price[],int idx,int n,vector<vector<int>>&dp){
+    int maxprofit(int price[],int arr[],int idx,int n,vector<vector<int>>&dp){
         if(n==0){
             return 0;
         }
@@ -18,7 +17,7 @@ class Solution{
             return dp[idx][n];
         }
         if(idx<=n){
-             dp[idx][n]=max(price[idx-1]+maxprofit(price,idx,n-idx,dp),maxprofit(price,idx+1,n,dp));
+             dp[idx][n]=max(price[idx-1]+maxprofit(price,arr,idx,n-arr[idx-1],dp),maxprofit(price,arr,idx+1,n,dp));
              return dp[idx][n];
         }
          dp[idx][n]=0;
@@ -26,10 +25,15 @@ class Solution{
     }
   public:
     int cutRod(int price[], int n) {
+        int arr[n];
+        for(int i=0;i<n;i++){
+            arr[i]=i+1;
+        }
         vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
-        return maxprofit(price,1,n,dp);
+        return maxprofit(price,arr,1,n,dp);
     }
 };
+
 
 //{ Driver Code Starts.
 
