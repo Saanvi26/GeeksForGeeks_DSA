ffda4b01ce97942c09b19c983ae2d21005362cf3
Saanvi Lakhanpal <129958210+Saanvi26@users.noreply.github.com>
2024-05-21 09:59:56 +0530

    Updated Solution - GfG to GitHub

--- Medium/Heap Sort/heap-sort.cpp
+++ Medium/Heap Sort/heap-sort.cpp
@@ -14,51 +14,43 @@ class Solution
     //Heapify function to maintain heap property.
     void heapify(int arr[], int n, int i)  
     {
-      // Your Code Here
+      int largest=i;
+      int left=2*i+1;
+      int right=2*i+2;
+      if(left<n && arr[largest]<arr[left]){
+          largest=left;
+      }
+      if(right<n && arr[largest]<arr[right]){
+           largest=right;
+      }
+      if(largest!=i){
+          swap(arr[largest],arr[i]);
+          heapify(arr,n,largest);
+      }
     }
 
     public:
     //Function to build a Heap from array.
     void buildHeap(int arr[], int n)  
     { 
-        for(int i=1;i<n;i++){
-            int p = (i-1)/2,c=i;
-            while(c and arr[c]>arr[p]){
-                swap(arr[c],arr[p]);
-                c=p;p=(c-1)/2;
-            }
-        }
+       for(int i=(n/2)-1;i>=0;i--){
+           heapify(arr,n,i);
+       }
     }
+
     
     public:
     //Function to sort an array using Heap Sort.
     void heapSort(int arr[], int n)
     {
         buildHeap(arr,n);
-        for(int i=n-1;i>0;i--){   // poping elements from heap
-            swap(arr[0],arr[i]);
-            int p=0;
-            while(p<i){
-                int l = 2*p+1,r=2*p+2;
-                if(l<i and r<i){
-                    int b = (arr[l]>arr[r])? l:r;
-                    if(arr[p]<arr[b]){
-                        swap(arr[p],arr[b]);
-                        p = b;
-                    }
-                    else break;
-                }
-                else if(l<i and arr[l]>arr[p]){
-                    swap(arr[p],arr[l]);
-                    p = l;
-                }
-                else if(r<i and arr[r]>arr[p]){
-                    swap(arr[p],arr[r]);
-                    p = r;
-                }
-                else break;
-    }
+        int size=n-1;
+        while(size>0){
+            swap(arr[0],arr[size]);
+            heapify(arr,size,0);
+             size--;
         }
+        
     }
 };
 
