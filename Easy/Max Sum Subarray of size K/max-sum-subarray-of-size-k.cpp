8157605c38d32721e7fc65939ed63fb4b5f5dd1d
Saanvi Lakhanpal <129958210+Saanvi26@users.noreply.github.com>
2024-05-22 14:15:11 +0530

    Updated Solution - GfG to GitHub

--- Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.cpp
+++ Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.cpp
@@ -6,19 +6,17 @@ using namespace std;
 class Solution{   
 public:
     long maximumSumSubarray(int K, vector<int> &Arr , int N){
-        long long int sum=0;
+        long sum=0;
         for(int i=0;i<K;i++){
             sum+=Arr[i];
         }
-          long long int ans=0;
-          ans=sum;
-        long long int sum2=sum;
-        for (int i=1;i<=N-K;i++){
-            sum2=sum2-Arr[i-1];
-            sum2+=Arr[K+i-1];
-            if(sum2 >ans){
-                ans=sum2;
-            }
+        long ans=sum;
+        int t=0;
+        for(int i=K;i<N;i++){
+            sum-=Arr[t];
+            t++;
+            sum+=Arr[i];
+            ans=max(ans,sum);
         }
         return ans;
     }
