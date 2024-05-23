7f31e37c03461a10558ca90cc1b8bc59a100a1f3
Saanvi Lakhanpal <129958210+Saanvi26@users.noreply.github.com>
2024-05-23 11:10:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First negative integer in every window of size k/first-negative-integer-in-every-window-of-size-k.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k);
+
+// Driver program to test above functions
+int main() {
+    long long int t, i;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        long long int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        long long int k;
+        cin >> k;
+
+        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+        for (auto it : ans) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+vector<long long> printFirstNegativeInteger(long long int A[],
+             long long int N, long long int K) {
+         deque<long long> neg;
+         vector<long long>ans;
+        int i=0;
+        for(int t=0;t<K;t++){
+            if(A[t]<0){
+                neg.push_back(A[t]);
+            }
+        }
+        int j=K;
+        if(neg.size()==0){
+            ans.push_back(0);
+        }
+        else{
+             ans.push_back(neg.front());
+        }
+        while(j<N){
+            if(A[j]<0){
+                neg.push_back(A[j]);
+            }
+            if(A[i]<0){
+                neg.pop_front();
+            }
+           if(neg.size()==0){
+            ans.push_back(0);
+            }
+           else{
+             ans.push_back(neg.front());
+            }
+            i++;
+            j++;
+        }
+        
+        return ans;
+ }
