e1423509b1fb1a0207f5492e6dc9fd3fb4fc7f34
Saanvi Lakhanpal <129958210+Saanvi26@users.noreply.github.com>
2024-09-05 11:52:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimal Cost/minimal-cost.cpp
+++ Difficulty: Medium/Minimal Cost/minimal-cost.cpp
@@ -6,22 +6,21 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int helper(int index,vector<int>& arr, int& k,vector<int>&dp){
-      if(index==0) return 0;
-      if(dp[index]!=-1) return dp[index];
-      int minsteps=INT_MAX;
-      int jump=INT_MAX;
-      for(int j=1;j<=k;j++){
-          if(index-j>=0)
-          jump=helper(index-j,arr,k,dp)+abs(arr[index]-arr[index-j]);
-          minsteps=min(minsteps,jump);
-      }
-      return dp[index]=minsteps;
-      
-  }
+  int helper(vector<int>& height, int n,int index,int k,vector<int>&dp){
+        if(index>=(n-1)) return 0;
+        int ans=INT_MAX;
+        if(dp[index]!=-1) return dp[index];
+        for(int i=1;i<=k;i++){
+         int term=INT_MAX; 
+        if(index+i<n)
+        term=abs(height[index]-height[index+i])+helper(height,n,index+i,k,dp);
+        ans=min(ans,term);
+    }
+        return dp[index]=ans;
+}
     int minimizeCost(vector<int>& arr, int& k) {
         vector<int>dp(arr.size()+1,-1);
-        return helper(arr.size()-1,arr,k,dp);
+       return helper(arr,arr.size(),0,k,dp);
     }
 };
 
